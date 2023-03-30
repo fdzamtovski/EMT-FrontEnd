49a246850c0c9d94dba8816b79c256c539db5468
fdzamtovski <76556606+fdzamtovski@users.noreply.github.com>
2023-03-30 10:55:03 +0200

    baseURL change 2

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://emt-frontend-lab.herokuapp.com/',
+    baseURL: 'https://localhost:9091/api',
     headers: {
         'Access-Control-Allow-Origin': '*'
     }
